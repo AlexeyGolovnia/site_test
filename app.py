d354791ed1c3928d6a06f93086dea98266ae4125
AlexeyGolovnia <golovnia.alexey@gmail.com>
2024-06-17 08:39:25 +0000

    n1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # return 'Hello world!'
-    return render_template('index.html')
+    return render_template('bootstrap.html')
 
 @app.route('/portfolio')
 def portfolio():
