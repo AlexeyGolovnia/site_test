da867806b552cbb7cbd59ac2c71817e2eca09dc3
AlexeyGolovnia <golovnia.alexey@gmail.com>
2024-07-01 09:42:20 +0000

    s2

--- app.py
+++ app.py
@@ -2,6 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Проект 1",
+        "description": "Краткое описание проекта 1",
+        "date": "Январь 2022 - Март 2022",
+        "tools": "HTML, CSS, JavaScript, React",
+        "role": "Разработчик интерфейса",
+    },
+    {
+        "name": "Проект 2",
+        "description": "Краткое описание проекта 2",
+        "date": "Июнь 2021 - Август 2021",
+        "tools": "Python, Django",
+        "role": "Полный стек разработчик",
+    }
+]
+
 @app.route('/')
 def home():
     # return 'Hello world!'
@@ -9,7 +26,7 @@ def home():
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    return render_template('portfolio.html', projects=projects)
 
 @app.route('/contacts')
 def contacts():
