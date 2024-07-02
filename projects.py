fd7d7f362235a0a4933bb34b014dfa63eb3040e0
AlexeyGolovnia <golovnia.alexey@gmail.com>
2024-07-02 12:10:00 +0000

    a3

--- projects.py
+++ projects.py
@@ -7,8 +7,7 @@ projects = [
     These dashboards provided a real-time overview of key production metrics, empowering leadership to monitor performance indicators and swiftly respond to any fluctuations or emerging issues.\
     This initiative significantly improved decision-making processes and operational efficiency by ensuring that critical data was always accessible and actionable.",
         "date": "Январь 2022,Март 2022",
-        "tools": "- Python,- Pandas,- Apache Airflow,- SQLAlchemy,- Seaborn,- Matplotlib",
-        # "role": "Разработчик интерфейса",
+        "tools": "- Python,- Pandas,- Apache Airflow,- SQLAlchemy,- Matplotlib",
     },
 
     {
@@ -18,7 +17,27 @@ projects = [
     This predictive model enabled proactive maintenance planning and resource allocation, ultimately reducing downtime and improving overall product reliability.",
         "date": "Июнь 2021,Август 2023",
         "tools": "- Python,- Scikit-learn,- PostgreSQL,- Pandas",
-        # "role": "Полный стек разработчик",
-    }
+    },
+
+    {
+        "name": "Document Video Registration",
+        "description": "\
+    I developed a real-time machine learning model that identifies document types using camera data.\
+    If the document type was initially undetermined, the model dynamically retrained itself on new data in real-time.\
+    Captured data was transmitted to a server for further user verification, enhancing document recognition accuracy and enabling secure and efficient user authentication processes.\
+    This adaptive learning approach ensured continuous improvement and reliability of document identification.",
+        "date": "Июнь 2021,Август 2023",
+        "tools": "- Python,- Pytorch,- OpenCV,- Multiprocessing,- Microsoft Azure",
+    },
+
+    {
+        "name": "Document Verification",
+        "description": "\
+    I developed a machine learning model that utilizes NLP techniques to determine the type of a document based on its content.\
+    This model automatically processes the document and transfers the data to a database for further handling and storage.\
+    By leveraging natural language processing, the system accurately categorizes documents, streamlining data management processes and ensuring efficient document verification workflows.",
+        "date": "Июнь 2021,Август 2023",
+        "tools": "- Pytorch,- Hugging Face,- Apache Airflow,- SQLAlchemy,- Microsoft Azure",
+    },
 
 ]
