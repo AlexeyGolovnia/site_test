46ea6644a35d91baf60a8ccc4afb989726b1131c
AlexeyGolovnia <golovnia.alexey@gmail.com>
2024-07-03 14:04:37 +0000

    d7

--- projects.py
+++ projects.py
@@ -1,23 +1,4 @@
 projects = [
-    
-    {
-        "name": "Information Dashboards",
-        "description": "\
-    I developed comprehensive information dashboards tailored for executive management.\
-    These dashboards provided a real-time overview of key production metrics, empowering leadership to monitor performance indicators and swiftly respond to any fluctuations or emerging issues.\
-    This initiative significantly improved decision-making processes and operational efficiency by ensuring that critical data was always accessible and actionable.",
-        "date": "May 2021,Jan 2022",
-        "tools": "- Python,- Pandas,- SQLAlchemy,- PostgreSQL,- Matplotlib",
-    },
-
-    {
-        "name": "Return Prediction",
-        "description": "\
-    I developed a machine learning model that predicted which type of product was most likely to require repairs in the current month based on historical data.\
-    This predictive model enabled proactive maintenance planning and resource allocation, ultimately reducing downtime and improving overall product reliability.",
-        "date": "Dec 2022,Feb 2023",
-        "tools": "- Python,- Scikit-learn,- PostgreSQL,- Pandas,- Matplotlib",
-    },
 
     {
         "name": "Document Video Registration",
@@ -26,7 +7,7 @@ projects = [
     If the document type was initially undetermined, the model dynamically retrained itself on new data in real-time.\
     Captured data was transmitted to a server for further user verification, enhancing document recognition accuracy and enabling secure and efficient user authentication processes.\
     This adaptive learning approach ensured continuous improvement and reliability of document identification.",
-        "date": "May 2024,Jun 2024",
+        "date": "Apr 2024,Jun 2024",
         "tools": "- Python,- Pytorch,- OpenCV,- Multiprocessing,- Microsoft Azure",
     },
 
@@ -36,9 +17,11 @@ projects = [
     I developed a machine learning model that utilizes NLP techniques to determine the type of a document based on its content.\
     This model automatically processes the document and transfers the data to a database for further handling and storage.\
     By leveraging natural language processing, the system accurately categorizes documents, streamlining data management processes and ensuring efficient document verification workflows.",
-        "date": "Feb 2025,Apr 2024",
+        "date": "Jan 2024,Mar 2024",
         "tools": "- Hugging Face,- Scikit-learn,- SQLAlchemy,- Pandas,- Microsoft Azure",
     },
+
+    #--------------------------------------------------------------------------------------------------
 
     {
         "name": "Business Process Refactoring",
@@ -63,13 +46,22 @@ projects = [
     },
 
     {
-        "name": "Archive Transformation",
+        "name": "Return Prediction",
         "description": "\
-    I developed a tool that converts old scanned technical PDF documents into electronic formats.\
-    Following conversion, the data was processed and stored in an SQL database for subsequent use in ERP processes.\
-    This initiative streamlined access to historical data, enhanced data management efficiency, and integrated legacy documentation into modern digital workflows, thereby improving overall operational effectiveness and facilitating informed decision-making in ERP environments.",
-        "date": "Jan 2020,Nov 2020",
-        "tools": "- Python,- Tkinter,- EasyOCR,- Apache Airflow,- PostgreSQL",
+    I developed a machine learning model that predicted which type of product was most likely to require repairs in the current month based on historical data.\
+    This predictive model enabled proactive maintenance planning and resource allocation, ultimately reducing downtime and improving overall product reliability.",
+        "date": "Dec 2022,Feb 2023",
+        "tools": "- Python,- Scikit-learn,- PostgreSQL,- Pandas,- Matplotlib",
+    },
+    
+    {
+        "name": "Information Dashboards",
+        "description": "\
+    I developed comprehensive information dashboards tailored for executive management.\
+    These dashboards provided a real-time overview of key production metrics, empowering leadership to monitor performance indicators and swiftly respond to any fluctuations or emerging issues.\
+    This initiative significantly improved decision-making processes and operational efficiency by ensuring that critical data was always accessible and actionable.",
+        "date": "May 2021,Jan 2022",
+        "tools": "- Python,- Pandas,- SQLAlchemy,- PostgreSQL,- Matplotlib",
     },
 
     {
@@ -80,6 +72,16 @@ projects = [
     The tool facilitated efficient data migration, ensuring continuity of operations and enhancing overall workflow automation and efficiency in document handling processes.",
         "date": "Sep 2020,Mar 2021",
         "tools": "- Python,- Lxml,- Pandas,- PostgreSQL,- SQLAlchemy",
+    },
+
+    {
+        "name": "Archive Transformation",
+        "description": "\
+    I developed a tool that converts old scanned technical PDF documents into electronic formats.\
+    Following conversion, the data was processed and stored in an SQL database for subsequent use in ERP processes.\
+    This initiative streamlined access to historical data, enhanced data management efficiency, and integrated legacy documentation into modern digital workflows, thereby improving overall operational effectiveness and facilitating informed decision-making in ERP environments.",
+        "date": "Jan 2020,Nov 2020",
+        "tools": "- Python,- Tkinter,- EasyOCR,- Apache Airflow,- PostgreSQL",
     },
 
     {
@@ -103,13 +105,15 @@ projects = [
     },
 
     {
-        "name": "Office Document Workflow",
+        "name": "Technological Document Workflow",
         "description": "\
-    I transitioned the enterprise's office document workflow to a digital format.\
-    This included implementing access controls, creating a user-friendly archive search, and enabling management to conveniently assign tasks, monitor their progress, and obtain detailed reports on any office document.\
-    This digital transformation improved efficiency, enhanced oversight, and provided comprehensive insights into document management and task tracking.",
-        "date": "Jan 2014,Feb 2015",
-        "tools": "- Intermech Search,- Intermech workflow,- Pymupdf,- Pandas,- Seaborn",
+    I automated the technological document workflow at the enterprise.\
+    I developed the necessary forms and approval processes for documentation.\
+    Essential directories were exported from the existing system.\
+    Once a technological document was created, all data were automatically transferred to the ERP system for further processing.\
+    This automation streamlined the workflow, ensuring seamless integration with the ERP system and enhancing overall efficiency in handling technological documentation.",
+        "date": "Jan 2016,Aug 2017",
+        "tools": "- Intermech Search,- Intermech workflow,- Intermech TechCard,- Intermech Imbase,- PostgreSQL",
     },
 
     {
@@ -125,46 +129,16 @@ projects = [
     },
 
     {
-        "name": "Technological Document Workflow",
+        "name": "Office Document Workflow",
         "description": "\
-    I automated the technological document workflow at the enterprise.\
-    I developed the necessary forms and approval processes for documentation.\
-    Essential directories were exported from the existing system.\
-    Once a technological document was created, all data were automatically transferred to the ERP system for further processing.\
-    This automation streamlined the workflow, ensuring seamless integration with the ERP system and enhancing overall efficiency in handling technological documentation.",
-        "date": "Jan 2016,Aug 2017",
-        "tools": "- Intermech Search,- Intermech workflow,- Intermech TechCard,- Intermech Imbase,- PostgreSQL",
+    I transitioned the enterprise's office document workflow to a digital format.\
+    This included implementing access controls, creating a user-friendly archive search, and enabling management to conveniently assign tasks, monitor their progress, and obtain detailed reports on any office document.\
+    This digital transformation improved efficiency, enhanced oversight, and provided comprehensive insights into document management and task tracking.",
+        "date": "Jan 2014,Feb 2015",
+        "tools": "- Intermech Search,- Intermech workflow,- Pymupdf,- Pandas,- Seaborn",
     },
 
-    {
-        "name": "<a href='https://github.com/AlexeyGolovnia/diary_events' target='_blank'>iPhone Daily Planner</a>",
-        "description": "\
-    I undertook a personal project using Swift to develop a daily planner app for my iPhone.\
-    I implemented all the necessary functionalities, including reminders and widgets.\
-    This self-developed app allowed me to move away from the limitations of existing applications, significantly enhancing the convenience of using my iPhone and boosting my productivity.",
-        "date": "Feb 2021,Apr 2021",
-        "tools": "- Swift,- SwiftUI,- WidgetKit,- UserNotifications",
-    },
-
-    {
-        "name": "<a href='https://github.com/AlexeyGolovnia/prometheus_project' target='_blank'>Website Monitoring</a>",
-        "description": "\
-    This was an educational project focused on tracking website visits with visualization in Grafana and notifications in Telegram.\
-    The solution was deployed in Docker, providing a comprehensive monitoring system that allowed for real-time tracking, insightful visualizations, and prompt notifications to ensure optimal website performance and user engagement.",
-        "date": "Nov 2022",
-        "tools": "- Prometheus,- Alertmanager,- Grafana,- Docker,- Telegram-bot",
-    },
-
-    {
-        "name": "<a href='https://github.com/AlexeyGolovnia/visa_center_bot' target='_blank'>Visa Slot Bot</a>",
-        "description": "\
-    I developed this project for personal use to secure a visa appointment.\
-    Available slots were quickly taken, and I didn't want to pay intermediaries.\
-    My program continuously monitored for available slots and sent notifications to my phone when slots opened up.\
-    This solution allowed me to efficiently secure an appointment without the need for third-party services.",
-        "date": "Aug 2023",
-        "tools": "- Python,- Selenium,- Telegram-bot",
-    },
+    #-------------------------------------------------------------------------------------------------------------
 
     {
         "name": "<a href='https://github.com/AlexeyGolovnia/site_test' target='_blank'>Portfolio Website</a>",
@@ -174,26 +148,6 @@ projects = [
     The website serves as a showcase of my skills and projects, providing a professional online presence that highlights my work and expertise.",
         "date": "Jul 2024",
         "tools": "- Python,- Flask,- Nginx,- Certbot,- Docker",
-    },
-
-    {
-        "name": "<a href='https://github.com/AlexeyGolovnia/digit_recognition' target='_blank'>Handwritten Text Recognition</a>",
-        "description": "\
-    I devised this educational project for myself to delve into the workings of a multilayer perceptron with backpropagation.\
-    Using only Numpy, I implemented a system for recognizing handwritten text without relying on other machine learning libraries.\
-    This project allowed me to gain a deeper understanding of neural networks and their applications in pattern recognition, reinforcing my proficiency in implementing machine learning algorithms from scratch.",
-        "date": "May 2021,Jun 2021",
-        "tools": "- Python,- Numpy,- Tkinter,- OpenCV",
-    },
-
-    {
-        "name": "<a href='https://github.com/AlexeyGolovnia/de_course' target='_blank'>Data Preparation</a>",
-        "description": "\
-    This educational project focuses on data preparation, involving key steps such as data preprocessing, integration of data from various sources, and populating a database with prepared data.\
-    The project is implemented using Apache Airflow, which orchestrates and automates these data pipeline tasks.\
-    By leveraging Airflow, I ensured efficient data management and integration, enhancing my skills in workflow orchestration and data engineering practices.",
-        "date": "Aug 2022,Oct 2022",
-        "tools": "- Python,- Apache Airflow,- PostgreSQL,- Sqlalchemy,- Docker",
     },
 
     {
@@ -214,6 +168,56 @@ projects = [
     This project sharpened my skills in data preprocessing, model selection, and ensemble learning techniques, demonstrating proficiency in leveraging machine learning for competitive data science challenges on Kaggle.",
         "date": "Nov 2023",
         "tools": "- Python,- Scikit-learn,- Pandas",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/visa_center_bot' target='_blank'>Visa Slot Bot</a>",
+        "description": "\
+    I developed this project for personal use to secure a visa appointment.\
+    Available slots were quickly taken, and I didn't want to pay intermediaries.\
+    My program continuously monitored for available slots and sent notifications to my phone when slots opened up.\
+    This solution allowed me to efficiently secure an appointment without the need for third-party services.",
+        "date": "Aug 2023",
+        "tools": "- Python,- Selenium,- Telegram-bot",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/prometheus_project' target='_blank'>Website Monitoring</a>",
+        "description": "\
+    This was an educational project focused on tracking website visits with visualization in Grafana and notifications in Telegram.\
+    The solution was deployed in Docker, providing a comprehensive monitoring system that allowed for real-time tracking, insightful visualizations, and prompt notifications to ensure optimal website performance and user engagement.",
+        "date": "Nov 2022",
+        "tools": "- Prometheus,- Alertmanager,- Grafana,- Docker,- Telegram-bot",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/de_course' target='_blank'>Data Preparation</a>",
+        "description": "\
+    This educational project focuses on data preparation, involving key steps such as data preprocessing, integration of data from various sources, and populating a database with prepared data.\
+    The project is implemented using Apache Airflow, which orchestrates and automates these data pipeline tasks.\
+    By leveraging Airflow, I ensured efficient data management and integration, enhancing my skills in workflow orchestration and data engineering practices.",
+        "date": "Aug 2022,Oct 2022",
+        "tools": "- Python,- Apache Airflow,- PostgreSQL,- Sqlalchemy,- Docker",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/digit_recognition' target='_blank'>Handwritten Text Recognition</a>",
+        "description": "\
+    I devised this educational project for myself to delve into the workings of a multilayer perceptron with backpropagation.\
+    Using only Numpy, I implemented a system for recognizing handwritten text without relying on other machine learning libraries.\
+    This project allowed me to gain a deeper understanding of neural networks and their applications in pattern recognition, reinforcing my proficiency in implementing machine learning algorithms from scratch.",
+        "date": "May 2021,Jun 2021",
+        "tools": "- Python,- Numpy,- Tkinter,- OpenCV",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/diary_events' target='_blank'>iPhone Daily Planner</a>",
+        "description": "\
+    I undertook a personal project using Swift to develop a daily planner app for my iPhone.\
+    I implemented all the necessary functionalities, including reminders and widgets.\
+    This self-developed app allowed me to move away from the limitations of existing applications, significantly enhancing the convenience of using my iPhone and boosting my productivity.",
+        "date": "Feb 2021,Apr 2021",
+        "tools": "- Swift,- SwiftUI,- WidgetKit,- UserNotifications",
     },
 
 ]
