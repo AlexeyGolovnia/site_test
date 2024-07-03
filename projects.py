dc8966035f3f43e958bad3490f56fedada0c5276
AlexeyGolovnia <golovnia.alexey@gmail.com>
2024-07-03 13:45:56 +0000

    d6

--- projects.py
+++ projects.py
@@ -6,7 +6,7 @@ projects = [
     I developed comprehensive information dashboards tailored for executive management.\
     These dashboards provided a real-time overview of key production metrics, empowering leadership to monitor performance indicators and swiftly respond to any fluctuations or emerging issues.\
     This initiative significantly improved decision-making processes and operational efficiency by ensuring that critical data was always accessible and actionable.",
-        "date": "Январь 2022,Март 2022",
+        "date": "May 2021,Jan 2022",
         "tools": "- Python,- Pandas,- SQLAlchemy,- PostgreSQL,- Matplotlib",
     },
 
@@ -15,7 +15,7 @@ projects = [
         "description": "\
     I developed a machine learning model that predicted which type of product was most likely to require repairs in the current month based on historical data.\
     This predictive model enabled proactive maintenance planning and resource allocation, ultimately reducing downtime and improving overall product reliability.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Dec 2022,Feb 2023",
         "tools": "- Python,- Scikit-learn,- PostgreSQL,- Pandas,- Matplotlib",
     },
 
@@ -26,7 +26,7 @@ projects = [
     If the document type was initially undetermined, the model dynamically retrained itself on new data in real-time.\
     Captured data was transmitted to a server for further user verification, enhancing document recognition accuracy and enabling secure and efficient user authentication processes.\
     This adaptive learning approach ensured continuous improvement and reliability of document identification.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "May 2024,Jun 2024",
         "tools": "- Python,- Pytorch,- OpenCV,- Multiprocessing,- Microsoft Azure",
     },
 
@@ -36,7 +36,7 @@ projects = [
     I developed a machine learning model that utilizes NLP techniques to determine the type of a document based on its content.\
     This model automatically processes the document and transfers the data to a database for further handling and storage.\
     By leveraging natural language processing, the system accurately categorizes documents, streamlining data management processes and ensuring efficient document verification workflows.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Feb 2025,Apr 2024",
         "tools": "- Hugging Face,- Scikit-learn,- SQLAlchemy,- Pandas,- Microsoft Azure",
     },
 
@@ -47,7 +47,7 @@ projects = [
     The primary objective was to automate existing processes and eliminate paper-based document workflows.\
     Through comprehensive restructuring efforts, we successfully revamped all processes, implemented electronic document management, and achieved full automation.\
     This initiative streamlined operations, enhanced efficiency, and significantly reduced administrative overhead, marking a transformative shift towards modernized and agile business practices.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Jan 2018,Sep 2023",
         "tools": "Team Lead role",
     },
 
@@ -58,7 +58,7 @@ projects = [
     This automated system facilitated the migration of all engineering and technical documentation into digital formats, ensuring immediate access to crucial documents.\
     By implementing robust access controls, we bolstered system security, while the integrated platform significantly accelerated both the development and maintenance of documentation processes.\
     This initiative streamlined operations, fortified data protection, and markedly enhanced productivity across engineering and document management workflows.",    
-        "date": "Июнь 2021,Август 2023",
+        "date": "Jan 2014,Sep 2023",
         "tools": "Team Lead role",
     },
 
@@ -68,7 +68,7 @@ projects = [
     I developed a tool that converts old scanned technical PDF documents into electronic formats.\
     Following conversion, the data was processed and stored in an SQL database for subsequent use in ERP processes.\
     This initiative streamlined access to historical data, enhanced data management efficiency, and integrated legacy documentation into modern digital workflows, thereby improving overall operational effectiveness and facilitating informed decision-making in ERP environments.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Jan 2020,Nov 2020",
         "tools": "- Python,- Tkinter,- EasyOCR,- Apache Airflow,- PostgreSQL",
     },
 
@@ -78,7 +78,7 @@ projects = [
     I developed a tool to extract directories from an outdated proprietary system and automatically populate these data into a new system.\
     This seamless transition enabled the organization to smoothly adopt a new automated document management system.\
     The tool facilitated efficient data migration, ensuring continuity of operations and enhancing overall workflow automation and efficiency in document handling processes.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Sep 2020,Mar 2021",
         "tools": "- Python,- Lxml,- Pandas,- PostgreSQL,- SQLAlchemy",
     },
 
@@ -88,7 +88,7 @@ projects = [
     I developed a tool for logging user requests within our system.\
     This program allowed for tracking the resolution of requests at any stage, provided management with insights into unresolved issues, and optimized the time of support staff.\
     By streamlining the support process, the tool enhanced efficiency and ensured timely problem resolution, thereby improving overall user satisfaction and operational effectiveness.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Mar 2019,Jun 2019",
         "tools": "- Python,- Tkinter,- Apache Airflow,- PostgreSQL,- Docker",
     },
 
@@ -98,7 +98,7 @@ projects = [
     I participated in the development of a tool for generating production order forms.\
     My responsibilities included integrating information from external sources into the application and exporting data from the completed forms to databases.\
     This work enabled the automation of several critical business processes, significantly enhancing efficiency and streamlining the workflow in production order management.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Apr 2017,Mar 2019",
         "tools": "- Apache Airflow,- PostgreSQL,- SQLAlchemy,- Lxml,- Pandas",
     },
 
@@ -108,7 +108,7 @@ projects = [
     I transitioned the enterprise's office document workflow to a digital format.\
     This included implementing access controls, creating a user-friendly archive search, and enabling management to conveniently assign tasks, monitor their progress, and obtain detailed reports on any office document.\
     This digital transformation improved efficiency, enhanced oversight, and provided comprehensive insights into document management and task tracking.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Jan 2014,Feb 2015",
         "tools": "- Intermech Search,- Intermech workflow,- Pymupdf,- Pandas,- Seaborn",
     },
 
@@ -120,7 +120,7 @@ projects = [
     All engineers began working within a unified PDM system, with all documentation approval processes configured.\
     This initiative reduced development and approval times, minimized errors during design, and enabled tracking of any document at various stages of production preparation.\
     The automation significantly improved efficiency, accuracy, and transparency in the engineering workflow.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Dec 2014,Jan 2016",
         "tools": "- Intermech Search,- Intermech workflow,- Intermech CadMech,- Intermech Imbase,- Autodesk Inventor",
     },
 
@@ -132,7 +132,7 @@ projects = [
     Essential directories were exported from the existing system.\
     Once a technological document was created, all data were automatically transferred to the ERP system for further processing.\
     This automation streamlined the workflow, ensuring seamless integration with the ERP system and enhancing overall efficiency in handling technological documentation.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Jan 2016,Aug 2017",
         "tools": "- Intermech Search,- Intermech workflow,- Intermech TechCard,- Intermech Imbase,- PostgreSQL",
     },
 
@@ -142,7 +142,7 @@ projects = [
     I undertook a personal project using Swift to develop a daily planner app for my iPhone.\
     I implemented all the necessary functionalities, including reminders and widgets.\
     This self-developed app allowed me to move away from the limitations of existing applications, significantly enhancing the convenience of using my iPhone and boosting my productivity.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Feb 2021,Apr 2021",
         "tools": "- Swift,- SwiftUI,- WidgetKit,- UserNotifications",
     },
 
@@ -151,7 +151,7 @@ projects = [
         "description": "\
     This was an educational project focused on tracking website visits with visualization in Grafana and notifications in Telegram.\
     The solution was deployed in Docker, providing a comprehensive monitoring system that allowed for real-time tracking, insightful visualizations, and prompt notifications to ensure optimal website performance and user engagement.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Nov 2022",
         "tools": "- Prometheus,- Alertmanager,- Grafana,- Docker,- Telegram-bot",
     },
 
@@ -162,7 +162,7 @@ projects = [
     Available slots were quickly taken, and I didn't want to pay intermediaries.\
     My program continuously monitored for available slots and sent notifications to my phone when slots opened up.\
     This solution allowed me to efficiently secure an appointment without the need for third-party services.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Aug 2023",
         "tools": "- Python,- Selenium,- Telegram-bot",
     },
 
@@ -172,7 +172,7 @@ projects = [
     I developed this portfolio website as a promotional tool using Flask.\
     It is hosted on a remote virtual machine on Microsoft Azure and deployed using Docker.\
     The website serves as a showcase of my skills and projects, providing a professional online presence that highlights my work and expertise.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "Jul 2024",
         "tools": "- Python,- Flask,- Nginx,- Certbot,- Docker",
     },
 
@@ -182,8 +182,38 @@ projects = [
     I devised this educational project for myself to delve into the workings of a multilayer perceptron with backpropagation.\
     Using only Numpy, I implemented a system for recognizing handwritten text without relying on other machine learning libraries.\
     This project allowed me to gain a deeper understanding of neural networks and their applications in pattern recognition, reinforcing my proficiency in implementing machine learning algorithms from scratch.",
-        "date": "Июнь 2021,Август 2023",
+        "date": "May 2021,Jun 2021",
         "tools": "- Python,- Numpy,- Tkinter,- OpenCV",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/de_course' target='_blank'>Data Preparation</a>",
+        "description": "\
+    This educational project focuses on data preparation, involving key steps such as data preprocessing, integration of data from various sources, and populating a database with prepared data.\
+    The project is implemented using Apache Airflow, which orchestrates and automates these data pipeline tasks.\
+    By leveraging Airflow, I ensured efficient data management and integration, enhancing my skills in workflow orchestration and data engineering practices.",
+        "date": "Aug 2022,Oct 2022",
+        "tools": "- Python,- Apache Airflow,- PostgreSQL,- Sqlalchemy,- Docker",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/outflow-of-telecom-customers-prediction' target='_blank'>Spark Pipeline</a>",
+        "description": "\
+    This educational project involves building a data processing pipeline in PySpark and subsequently training a model with automatic hyperparameter tuning.\
+    The project utilizes PySpark to construct a robust pipeline that handles data preprocessing, feature engineering, model training, and evaluation.\
+    Automatic hyperparameter tuning ensures optimal model performance without manual intervention.",
+        "date": "Dec 2023",
+        "tools": "- Python,- Spark,- PySpark MLlib",
+    },
+
+    {
+        "name": "<a href='https://github.com/AlexeyGolovnia/revenue-prediction' target='_blank'>Kaggle Competition</a>",
+        "description": "\
+    In this Kaggle competition, I employed various techniques with basic machine learning models.\
+    I extensively utilized the scikit-learn library, implementing data processing pipelines, training multiple models with hyperparameter tuning, and selecting the optimal model through ensemble voting.\
+    This project sharpened my skills in data preprocessing, model selection, and ensemble learning techniques, demonstrating proficiency in leveraging machine learning for competitive data science challenges on Kaggle.",
+        "date": "Nov 2023",
+        "tools": "- Python,- Scikit-learn,- Pandas",
     },
 
 ]
